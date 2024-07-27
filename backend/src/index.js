268ef0ded2f8d2bb4f056dab34bc22a7f00cb717
Thanh Huynh <thanh.huynh@gc.com>
2024-07-27 14:08:30 -0400

    set up mongo

---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+
+dotenv.config();
+
+const startServer = async () => {
+  const app = express();
+
+  const server = new ApolloServer({ typeDefs, resolvers });
+  await server.start();
+  server.applyMiddleware({ app });
+
+  await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  app.listen({ port: process.env.PORT }, () =>
+    console.log(`🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`)
+  );
+};
+
+startServer();
